07d9080f99720b12abad8e9d7531276d268fe4d2
tapholer <tapholer@protonmail.com>
2018-02-18 17:48:19 -0800

    Initial Spring Boot Framework

---
+++ tapholes/src/main/java/com/engine/tapholes/TapholesApplication.java
@@ -0,0 +1,12 @@
+package com.engine.tapholes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapholesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapholesApplication.class, args);
+	}
+}
---
+++ tapholes/src/test/java/com/engine/tapholes/TapholesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.engine.tapholes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TapholesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
